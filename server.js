5e189439ad534e603f5f063dff1f29118e10e80e
macsta31 <mackstathis@gmail.com>
2023-05-11 13:38:42 -0400

    breaches route / robust error messages

--- server.js
+++ server.js
@@ -17,15 +17,49 @@ app.get('/breachedaccount/:email', async (req, res) => {
     const response = await axios.get(`https://haveibeenpwned.com/api/v3/breachedaccount/${email}?truncateResponse=false`, {
       headers: {
         'User-Agent': 'YourAppName',
-        // Add your API key, if you have one
         'hibp-api-key': 'd6c5fbdd8570474f90ac68b6f17e61f4',
       },
     });
     res.json(response.data);
   } catch (error) {
-    res.status(500).json({ message: 'Error fetching data from HaveIBeenPwned API' });
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      res.status(error.response.status).json({message: `Error ${error.response.statusText}`})
+    } else if (error.request) {
+      // The request was made but no response was received
+      res.status(500).json({message: `No response was received from the server`})
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      res.status(500).json({ message: `Error: ${error.message}`});
+    }
   }
 });
+
+app.get('/breaches/:domain', async (req, res) => {
+  const domain = req.params.domain;
+  try{
+    const response = await axios.get(`https://haveibeenpwned.com/api/v3/breaches?domain=${domain}&truncateResponse=false`, {
+      headers: {
+        'User-Agent': 'YourAppName',
+        'hibp-api-key': 'd6c5fbdd8570474f90ac68b6f17e61f4',
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      res.status(error.response.status).json({message: `Error ${error.response.statusText}`})
+    } else if (error.request) {
+      // The request was made but no response was received
+      res.status(500).json({message: `No response was received from the server`})
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      res.status(500).json({ message: `Error: ${error.message}`});
+    }
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
