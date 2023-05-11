aed9794186bf4651be4e38bba3afb6b902b35b54
macsta31 <mackstathis@gmail.com>
2023-05-11 16:59:23 -0400

    conversion to serverless for vercel deployment

---
+++ api/breaches.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  try {
+    const domain = req.query.domain;
+    const response = await axios.get(`https://haveibeenpwned.com/api/v3/breaches?domain=${domain}&truncateResponse=false`, {
+      headers: {
+        'User-Agent': 'YourAppName',
+        'hibp-api-key': 'd6c5fbdd8570474f90ac68b6f17e61f4',
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    handleError(error, res);
+  }
+};
+
+function handleError(error, res) {
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    res.status(error.response.status).json({message: `Error ${error.response.statusText}`})
+  } else if (error.request) {
+    // The request was made but no response was received
+    res.status(500).json({message: `No response was received from the server`})
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    res.status(500).json({ message: `Error: ${error.message}`});
+  }
+}
